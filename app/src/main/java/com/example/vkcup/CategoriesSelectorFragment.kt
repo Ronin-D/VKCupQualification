5efaa6e91f0ad26e1fcc9226b34c004b884dae00
Ronin <yato050303@rambler.ru>
2022-12-06 22:21:18 +1000

    bug fix added calculating the columns count based on screen size

--- app/src/main/java/com/example/vkcup/CategoriesSelectorFragment.kt
+++ app/src/main/java/com/example/vkcup/CategoriesSelectorFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewManager
+import android.view.ViewTreeObserver
 import android.widget.CheckBox
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
@@ -29,28 +30,61 @@ class CategoriesSelectorFragment : Fragment() {
         return binding.root
     }
 
+    //высчитывать ширину экрана и на ее основе делать количество колонок в лайаутах
+  /*
 
+   */
 
     override fun onStart() {
         super.onStart()
 
+        binding.unpressedTagsGridlayout.viewTreeObserver.addOnGlobalLayoutListener(
+            object :ViewTreeObserver.OnGlobalLayoutListener{
+                override fun onGlobalLayout() {
+                    binding.unpressedTagsGridlayout.viewTreeObserver
+                        .removeOnGlobalLayoutListener(this)
+                    var columnCount = binding.unpressedTagsGridlayout
+                        .width/(2*binding.humorTagCheckBox.width)
+                    if (columnCount<1){
+                        columnCount = 1
+                    }
+                    binding.unpressedTagsGridlayout.columnCount = columnCount
+                    binding.pressedTagsGridlayout.columnCount = columnCount
+                }
+
+            }
+        )
         checkTagState(binding.carsTagCheckBox)
         checkTagState(binding.foodTagCheckBox)
-        checkTagState(binding.checkBox41)
-        checkTagState(binding.restaurantsTagCheckBox2)
+        checkTagState(binding.humorTagCheckBox)
+        checkTagState(binding.restaurantsTagCheckBox)
         checkTagState(binding.politicsTagCheckBox)
+        checkTagState(binding.sportTagCheckBox)
+        checkTagState(binding.serialsTagCheckBox)
+        checkTagState(binding.newsTagCheckBox)
+        checkTagState(binding.walksTagCheckBox)
+        checkTagState(binding.jobTagCheckBox)
+        checkTagState(binding.restTagCheckBox)
+        checkTagState(binding.recipesTagCheckBox)
         checkTagState(binding.filmsTagCheckBox)
 
-        binding.foodTagCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
-            replaceTag(binding.foodTagCheckBox,isChecked)
+
+        binding.foodTagCheckBox.apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
         }
 
-        binding.carsTagCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
-           replaceTag(binding.carsTagCheckBox,isChecked)
+        binding.carsTagCheckBox.apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
         }
 
-        binding.filmsTagCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
-            replaceTag(binding.filmsTagCheckBox,isChecked)
+        binding.filmsTagCheckBox.apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
         }
 
         binding.politicsTagCheckBox.apply {
@@ -59,22 +93,57 @@ class CategoriesSelectorFragment : Fragment() {
             }
         }
 
-          binding.restaurantsTagCheckBox2 .apply {
+          binding.restaurantsTagCheckBox .apply {
             setOnCheckedChangeListener { buttonView, isChecked ->
                 replaceTag(this,isChecked)
             }
         }
-        binding.checkBox41 .apply {
+        binding.newsTagCheckBox .apply {
             setOnCheckedChangeListener { buttonView, isChecked ->
                 replaceTag(this,isChecked)
             }
         }
 
+        binding.serialsTagCheckBox .apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
+        }
+        binding.sportTagCheckBox .apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
+        }
+        binding.walksTagCheckBox .apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
+        }
+        binding.restTagCheckBox .apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
+        }
+        binding.jobTagCheckBox .apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
+        }
+        binding.recipesTagCheckBox .apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
+        }
+        binding.humorTagCheckBox.apply {
+            setOnCheckedChangeListener { buttonView, isChecked ->
+                replaceTag(this,isChecked)
+            }
+        }
 
     }
 
     private fun replaceTag(view: View, isChecked:Boolean){
-        val animationDuration = resources.getInteger(android.R.integer.config_longAnimTime)
+        val animationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
         if (isChecked) {
             view.animate().apply {
                 alpha(0f)
@@ -83,7 +152,7 @@ class CategoriesSelectorFragment : Fragment() {
                     override fun onAnimationEnd(animation: Animator) {
                         alpha(1f)
                         binding.unpressedTagsGridlayout.removeView(view)
-                        binding.linear.addView(view,0)
+                        binding.pressedTagsGridlayout.addView(view,0)
                         view.animate().setListener(null)
                     }
                 })
@@ -98,7 +167,7 @@ class CategoriesSelectorFragment : Fragment() {
                 setListener(object : AnimatorListenerAdapter() {
                     override fun onAnimationEnd(animation: Animator) {
                         alpha(1f)
-                        binding.linear.removeView(view)
+                        binding.pressedTagsGridlayout.removeView(view)
                         binding.unpressedTagsGridlayout.addView(view,0)
                         view.animate().setListener(null)
                     }
@@ -108,9 +177,9 @@ class CategoriesSelectorFragment : Fragment() {
     }
 
     private fun checkTagState(checkBox: CheckBox){
-        if(checkBox.isChecked){
+        if(checkBox.isChecked&&checkBox.parent==binding.unpressedTagsGridlayout){
             binding.unpressedTagsGridlayout.removeView(checkBox)
-            binding.linear.addView(checkBox,0)
+            binding.pressedTagsGridlayout.addView(checkBox,0)
         }
     }
 
